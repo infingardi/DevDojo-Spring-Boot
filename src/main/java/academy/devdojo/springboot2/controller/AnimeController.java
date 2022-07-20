9d7938573ea109b6178a28e0b0ced2d10933f5de
Igor <infingardidev@gmail.com>
2022-07-20 12:42:09 -0300

    iniciando projeto java Spring Boot DevDojo

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,31 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime") // localhost:8080/anime
+@Log4j2
+// @AllArgsConstructor cria um construtor com todos os atributos da classe
+// @RequiredArgsConstructor cria um construtor com todos os atributos final da classe
+public class AnimeController {
+    private DateUtil dateUtil;
+
+    public AnimeController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    // localhost:8080/anime/list
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("erased"), new Anime("Berserker"));
+    }
+}
